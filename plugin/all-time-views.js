88661ceeb76862641e13e9bcf41b0dc06f0740f5
Jeff Doktor <jdoktor@brightcove.com>
2018-02-26 16:20:16 -0700

    update to new proxy

--- plugin/all-time-views.js
+++ plugin/all-time-views.js
@@ -6,6 +6,8 @@ videojs.plugin('alltimeViews', function(options) {
     alltimeViewsEl,
     videoElement = document.getElementById('bcls_alltimePlaysPlayer'),
     account_id = videoElement.getAttribute('data-account'),
+    baseURL = "https://analytics.api.brightcove.com/v1/alltime/accounts/";
+    options.requestType = "GET";
 
     // listener for the first video load
     player.one('loadedmetadata', function() {
@@ -54,72 +56,78 @@ videojs.plugin('alltimeViews', function(options) {
    * createRequest sets up requests, send them to makeRequest(), and handles responses
    * @param  {string} type the request type
    */
-  function createRequest() {
-    var requestOptions = {},
-      responseDecoded;
+   function createRequest() {
+     var JSONresponse;
 
-    // set the proxy url
-    requestOptions.proxyURL = options.proxyURL;
-    // set the Analytics API request
-    requestOptions.url = 'https://analytics.api.brightcove.com/v1/alltime/accounts/' + account_id + '/videos/' + video_id;
-    console.log('requestOptions', requestOptions);
-    // send the request and handle the response
-    makeRequest(requestOptions, function(response) {
-      // parse the response
-      responseDecoded = JSON.parse(response);
-      // should always get a response, but just in case...
-      if (responseDecoded && responseDecoded.alltime_video_views) {
-        // write the views to the UI
-        alltimeViewsEl.textContent = numberWithCommas(responseDecoded.alltime_video_views) + ' views';
-      } else {
-        alltimeViewsEl.textContent = 'All-time views for this video are not available';
-      }
-    });
-  }
+     // set the Analytics API request
+     options.url = baseURL + account_id + '/videos/' + video_id;
+     // send the request and handle the response
+     makeRequest(options, function (response) {
+     // Convert response string into JSON
+     JSONresponse = JSON.parse(response);
+       // should always get a response, but just in case...
+       if (JSONresponse && JSONresponse.alltime_video_views) {
+         // write the views to the UI
+         alltimeViewsEl.textContent = numberWithCommas(JSONresponse.alltime_video_views) + ' views';
+       } else {
+         alltimeViewsEl.textContent = 'All-time views for this video are not available';
+       }
+     });
+   }
 
 
   // +++ Make actual call to proxy/Analytics API +++
-  /**
-   * sends Brightcove API requests to a server-side proxy
-   * @param  {Object} requestOptions for the request
-   * @param  {String} requestOptions.url the full API request URL
-   * @param  {String="GET","POST","PATCH","PUT","DELETE"} requestData [options.requestType="GET"] HTTP type for the request
-   * @param  {String} requestOptions.proxyURL proxyURL to send the request to
-   * @param  {Function} [callback] callback function that will process the response
-   */
-  function makeRequest(requestOptions, callback) {
+/**
+* send API request to the proxy
+* @param  {Object} options for the request
+* @param  {String} options.url the full API request URL
+* @param  {String="GET","POST","PATCH","PUT","DELETE"} requestData [options.requestType="GET"] HTTP type for the request
+* @param  {String} options.proxyURL proxyURL to send the request to
+* @param  {String} options.client_id client id for the account (default is in the proxy)
+* @param  {String} options.client_secret client secret for the account (default is in the proxy)
+* @param  {JSON} [options.requestBody] Data to be sent in the request body in the form of a JSON string
+* @param  {Function} [callback] callback function that will process the response
+*/
+  function makeRequest(options, callback) {
     var httpRequest = new XMLHttpRequest(),
-      response,
-      requestParams,
-      proxyURL = requestOptions.proxyURL,
-      // response handler
-      getResponse = function() {
-        try {
-          if (httpRequest.readyState === 4) {
-            if (httpRequest.status === 200) {
-              response = httpRequest.responseText;
-              // some API requests return '{null}' for empty responses - breaks JSON.parse
-              if (response === '{null}') {
-                response = null;
+        response,
+        requestParams,
+        dataString,
+        proxyURL = options.proxyURL,
+        // response handler
+        getResponse = function() {
+          try {
+            if (httpRequest.readyState === 4) {
+              if (httpRequest.status >= 200 && httpRequest.status < 300) {
+                response = httpRequest.responseText;
+                // some API requests return '{null}' for empty responses - breaks JSON.parse
+                if (response === "{null}") {
+                  response = null;
+                }
+                // return the response
+                callback(response);
+              } else {
+                alert(
+                  "There was a problem with the request. Request returned " +
+                  httpRequest.status
+                );
               }
-              // return the response
-              callback(response);
-            } else {
-              alert('There was a problem with the request. Request returned ' + httpRequest.status);
             }
+          } catch (e) {
+            alert("Caught Exception: " + e);
           }
-        } catch (e) {
-          alert('Caught Exception: ' + e);
-        }
-      };
-    requestParams = 'url=' + encodeURIComponent(requestOptions.url) + '&requestType=GET';
+        };
+     /**
+       * set up request data
+         * the proxy used here takes the following request body:
+         * JSON.strinify(options)
+         */
     // set response handler
     httpRequest.onreadystatechange = getResponse;
     // open the request
-    httpRequest.open('POST', proxyURL);
-    // set headers
-    httpRequest.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+    httpRequest.open("POST", proxyURL);
+    // set headers if there is a set header line, remove it
     // open and send request
-    httpRequest.send(requestParams);
+    httpRequest.send(JSON.stringify(options));
   }
 });
